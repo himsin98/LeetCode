37e6fdbf10288df2f7daeb926925d9b06f623805
himsin98 <88057383+himsin98@users.noreply.github.com>
2022-01-06 12:12:07 +0530

    Time: 5 ms (33.74%), Space: 49.5 MB (8.45%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
@@ -14,20 +14,16 @@
  * }
  */
 class Solution {
-    public boolean findTarget(TreeNode root, int k) {
-        var set = new HashSet<Integer>();
-        var q = new LinkedList<TreeNode>();
-        q.add(root);
-        while(! q.isEmpty()){
-            var node = q.poll();
-            if(node == null)
-                continue;
-            if(set.contains(k-node.val))
-                return true;
-            set.add(node.val);
-            q.add(node.left);
-            q.add(node.right);
-        }
-        return false;
+    private Set<Integer> set = new HashSet<>();
+    public boolean findTarget(TreeNode root, int target) {
+        return inOrder(root, target);
+    }
+    private boolean inOrder(TreeNode root, int target){
+        if(root == null)
+            return false;
+        if(set.contains(target-root.val))
+            return true;
+        set.add(root.val);
+        return inOrder(root.left, target) || inOrder(root.right, target);
     }
 }
