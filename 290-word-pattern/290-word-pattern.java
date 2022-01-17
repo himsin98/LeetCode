f144c0e34c6d4187c20f295acc255acbfc3af88f
himsin98 <88057383+himsin98@users.noreply.github.com>
2022-01-17 11:19:21 +0530

    Time: 0 ms (100.00%), Space: 37 MB (63.98%) - LeetHub

--- 290-word-pattern/290-word-pattern.java
+++ 290-word-pattern/290-word-pattern.java
@@ -1,21 +1,23 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
-	String[] strArr = s.split(" ");
-	if (pattern.length() != strArr.length) return false;
-	Map<String, Character> sToPatternDic = new HashMap<String, Character>();
-	Map<Character, String> patternToSDic = new HashMap<Character, String>();
-	for (int i = 0; i < strArr.length; i++) {
-		if (sToPatternDic.containsKey(strArr[i])) {
-			if (sToPatternDic.get(strArr[i]) != pattern.charAt(i)) return false;
-		} else {
-			sToPatternDic.put(strArr[i], pattern.charAt(i));
-		}
-		if (patternToSDic.containsKey(pattern.charAt(i))) {
-			if (!Objects.equals(patternToSDic.get(pattern.charAt(i)),strArr[i])) return false;
-		} else {
-			patternToSDic.put(pattern.charAt(i), strArr[i]);
-		}
-	}
-	return true;
-}
+	    String[] arr = s.split(" ", 0);
+        Map<Character, String> map = new HashMap<>();
+        Map<String, Character> map2 = new HashMap<>();
+        if(arr.length != pattern.length())
+            return false;
+        for(int i = 0; i< pattern.length(); i++){
+            if(!map.containsKey(pattern.charAt(i))){
+                if(!map2.containsKey(arr[i])){
+                    map.put(pattern.charAt(i), arr[i]);
+                    map2.put(arr[i], pattern.charAt(i));
+                }else{
+                    return false;
+                }
+                
+            }else if(!map.get(pattern.charAt(i)).equals(arr[i])){
+                return false;
+            }
+        }
+        return true;
+    }
 }
