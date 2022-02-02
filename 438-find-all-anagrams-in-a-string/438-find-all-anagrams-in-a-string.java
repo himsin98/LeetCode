cb87a7001e1697981080d726524cf141ae5887cc
himsin98 <88057383+himsin98@users.noreply.github.com>
2022-02-02 17:18:51 +0530

    Time: 18 ms (54.80%), Space: 47.1 MB (19.14%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
@@ -1,24 +1,32 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> list = new ArrayList<>();
-        for(int i = 0; i<s.length()+1-p.length(); i++){
-            if(isAnagram(s.substring(i,i+p.length()), p)){
-                list.add(i);
-            }
+        List<Integer> res = new ArrayList<>();
+        if(s.length() < p.length()){
+            return res;
         }
-        return list;
+        int[] target = new int[26];
+        for(char i : p.toCharArray())
+            target[i-'a']++;
+        char[] array = s.toCharArray();
+        int[] buffer = go(array, p.length());
+        if(Arrays.equals(buffer, target))
+            res.add(0);
+        
+        for(int i = p.length(); i<array.length; i++){
+            buffer[array[i-p.length()]-'a']--;
+            buffer[array[i] - 'a']++;
+            if(Arrays.equals(target, buffer))
+                res.add(i-p.length()+1);
+        }
+        return res;
+        
     }
     
-    private boolean isAnagram(String s, String p){
-        int[] arr = new int[26];
-            for(int i = 0; i<s.length(); i++){
-                arr[s.charAt(i)-'a']++;
-                arr[p.charAt(i)-'a']--;
-            }
-            for(int i:arr){
-                if(i != 0)
-                    return false;
-            }
-            return true;
+    private int[] go(char[] array, int size){
+        int[] buffer = new int[26];
+        for(int i = 0; i<size; i++)
+            buffer[array[i] - 'a']++;
+        return buffer;
     }
+    
 }
