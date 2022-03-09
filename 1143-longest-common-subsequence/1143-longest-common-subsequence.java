803e8b9a41cdf50afa8e42fe0f4bbb0fe9fcd465
Himanshu Singh <himsin98@gmail.com>
2022-03-09 12:51:01 +0530

    Time: 36 ms (15.25%), Space: 46.2 MB (44.10%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,21 +1,25 @@
 class Solution {
     public int longestCommonSubsequence(String s, String t) {
-        String[][] mem = new String[s.length()][t.length()];
-        return lcs(s, t, 0, 0, mem).length();
+        int[][] mem = new int[s.length()][t.length()];
+        for(int i = 0; i< s.length(); i++){
+            Arrays.fill(mem[i], -1);
+        }
+        
+        return lcs(s, t, 0, 0, mem);
     }
     
-    private String lcs(String s1, String s2, int l1, int l2, String[][] mem){
+    private int lcs(String s1, String s2, int l1, int l2, int[][] mem){
         if(l1 == s1.length() || l2 == s2.length())
-            return "";
-        if(mem[l1][l2] != null)
+            return 0;
+        if(mem[l1][l2] != -1)
             return mem[l1][l2];
         
         if(s1.charAt(l1) == s2.charAt(l2)){
             mem[l1][l2] = 1 + lcs(s1, s2, l1+1, l2+1, mem);
         }else{
-            String s = lcs(s1, s2, l1+1, l2, mem);
-            String t = lcs(s1, s2, l1, l2+1, mem);
-            mem[l1][l2] = s.length() > t.length()?s:t;
+            //String s = lcs(s1, s2, l1+1, l2, mem);
+            //String t = lcs(s1, s2, l1, l2+1, mem);
+            mem[l1][l2] = Math.max(lcs(s1, s2, l1+1, l2, mem), lcs(s1, s2, l1, l2+1, mem));
         }
         return mem[l1][l2];
     }
